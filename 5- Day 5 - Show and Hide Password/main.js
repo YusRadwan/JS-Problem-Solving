404d619a6f58e742619489279d95d8b2bd1bf6ea
YRadwan <43502481+YusRadwan@users.noreply.github.com>
2025-12-11 00:05:08 +0200

    change Emoji when try to see password

--- 5- Day 5 - Show and Hide Password/main.js
+++ 5- Day 5 - Show and Hide Password/main.js
@@ -2,8 +2,12 @@ const inputPass = document.querySelector("#passes");
 const showIcon = document.querySelector("#eyeicon");
 
 showIcon.addEventListener("mousedown", (e) => {
-  if (inputPass.type === "password") inputPass.type = "text";
+  if (inputPass.type === "password") {
+    inputPass.type = "text";
+    showIcon.textContent = "👀";
+  }
   addEventListener("mouseup", () => {
     if (inputPass.type === "text") inputPass.type = "password";
+    showIcon.textContent = "🫣";
   });
 });
