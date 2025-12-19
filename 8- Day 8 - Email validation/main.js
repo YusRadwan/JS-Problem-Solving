8354d26a33429b1be842733647060805d54f88fc
usufradwan99@gmail.com <43502481+YusRadwan@users.noreply.github.com>
2025-12-19 22:36:40 +0200

    Update Email Validation App

--- 8- Day 8 - Email validation/main.js
+++ 8- Day 8 - Email validation/main.js
@@ -1,21 +1,41 @@
 console.log(`%%% Email validation App %%%`);
 
+// Call Element
 const emailForm = document.getElementById("emailForm");
 const errorMessage = document.getElementById("error-message");
 const submit = document.querySelector("button");
 
-submit.addEventListener("click", async (e) => {
+// Validation
+function validEmail(email) {
+  const pattern = /^[\w-]+@([\w-]+\.)+[\w-]{2,4}$/;
+  return pattern.test(email);
+}
+
+// Updates UI
+function showError() {
+  errorMessage.style.display = "block";
+  errorMessage.textContent = `Please enter a valid email ...!`;
+}
+
+function hideError() {
+  errorMessage.style.display = "none";
+  document.getElementById("email").value = ''
+}
+
+// Event listener
+emailForm.addEventListener("submit", (e) => {
   // Stop Action
   e.preventDefault();
-  const email = document.getElementById("email").value;
-  const pattern = /^[\w-]+@([\w-]+\.)+[\w-]{2,4}$/g;
 
-  // Check
-  if (!pattern.test(email)) {
-    errorMessage.style.display = "block";
-    errorMessage.textContent = `Please enter a valid email ...!`;
-  } else {
-    errorMessage.style.display = "none";
-    emailForm.reset();
+  // Call Value input Email
+  const email = document.getElementById("email").value.trim().toLowerCase();
+
+  // Check if Valid
+  if (!validEmail(email)) {
+    showError();
+    return;
   }
+
+  // Hidden Message Error if Email valid
+  hideError();
 });
